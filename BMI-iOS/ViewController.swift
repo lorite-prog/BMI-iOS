cbd96b299db821eabae54efb3260d594465a6983
lorite-prog <loritemarcos@gmail.com>
2026-01-14 17:43:57 +0100

    Cambios

--- BMI-iOS/ViewController.swift
+++ BMI-iOS/ViewController.swift
@@ -12,10 +12,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var HorizontalSl: UISlider!
     @IBOutlet weak var Altura: UILabel!
     @IBOutlet weak var Peso: UILabel!
-    
     @IBOutlet weak var resultIMC: UILabel!
     @IBOutlet weak var MasMenos: UIStepper!
     @IBOutlet weak var CalculateButton: UIButton!
+    @IBOutlet weak var Description: UILabel!
+    
     
     var peso: Float = 60.0
     var altura: Float = 170.0
@@ -30,8 +31,31 @@ class ViewController: UIViewController {
         let imc = peso / (alturaenm * alturaenm)
         print("El IMC es: \(imc)")
         resultIMC.text = "\(imc)"
-        let formatedResult = String(format: "%.2f", imc)
         
+        resultIMC.text = String(format: "%.2f", imc)
+        
+        switch imc {
+        case 0..<18,5:
+            Description.text = "Bajo peso"
+            resultIMC.textColor = UIColor.imcBajopeso
+            Description.textColor = UIColor.imcBajopeso
+        case 18,5..<25:
+            Description.text = "Peso normal"
+            resultIMC.textColor = UIColor.imcPesonormal
+            Description.textColor = UIColor.imcPesonormal
+        case 25..<30:
+            Description.text = "Sobrepeso"
+            resultIMC.textColor = UIColor.imcSobrepeso
+            Description.textColor = UIColor.imcSobrepeso
+        case 30..<40:
+            Description.text = "Obesidad"
+            resultIMC.textColor = UIColor.imcObesidad
+            Description.textColor = UIColor.imcObesidad
+        default:
+            Description.text = "Obesidad grave"
+            resultIMC.textColor = UIColor.imcObesidadgrave
+            Description .textColor = UIColor.imcObesidadgrave
+        }
         
     }
     @IBAction func pesoChanged(_ sender: Any) {
